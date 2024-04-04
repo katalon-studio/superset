a65de0f42e91e5415be3c181e52b1aaf94027443
Trai Le <trai.le@katalon.com>
2024-04-04 11:58:22 +0700

    Duplicate code to prevent confict code in future

--- superset-frontend/src/katalon/CustomFilter/KatalonHorizontal.tsx
+++ superset-frontend/src/katalon/CustomFilter/KatalonHorizontal.tsx
@@ -1,22 +1,3 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 import React, { useMemo } from 'react';
 import {
   DataMaskStateWithId,
@@ -28,14 +9,19 @@ import {
 } from '@superset-ui/core';
 import Icons from 'src/components/Icons';
 import Loading from 'src/components/Loading';
-import { DashboardLayout, RootState } from 'src/dashboard/types';
 import { useSelector } from 'react-redux';
-import FilterControls from './FilterControls/FilterControls';
-import { useChartsVerboseMaps, getFilterBarTestId } from './utils';
-import { HorizontalBarProps } from './types';
-import FilterBarSettings from './FilterBarSettings';
-import FilterConfigurationLink from './FilterConfigurationLink';
-import crossFiltersSelector from './CrossFilters/selectors';
+import {
+  getFilterBarTestId,
+  useChartsVerboseMaps,
+} from 'src/dashboard/components/nativeFilters/FilterBar/utils';
+import { HorizontalBarProps } from 'src/dashboard/components/nativeFilters/FilterBar/types';
+import { DashboardLayout, RootState } from 'src/dashboard/types';
+import { crossFiltersSelector } from 'src/dashboard/components/nativeFilters/FilterBar/CrossFilters/selectors';
+import { getUrlParam } from 'src/utils/urlUtils';
+import { URL_PARAMS } from 'src/constants';
+import FilterConfigurationLink from 'src/dashboard/components/nativeFilters/FilterBar/FilterConfigurationLink';
+import FilterBarSettings from 'src/dashboard/components/nativeFilters/FilterBar/FilterBarSettings';
+import FilterControls from 'src/dashboard/components/nativeFilters/FilterBar/FilterControls/FilterControls';
 
 const HorizontalBar = styled.div`
   ${({ theme }) => `
@@ -135,6 +121,8 @@ const HorizontalFilterBar: React.FC<HorizontalBarProps> = ({
     [actions],
   );
 
+  const isKatalonEmbeddedMode = getUrlParam(URL_PARAMS.isKatalonEmbeddedMode);
+
   return (
     <HorizontalBar {...getFilterBarTestId()}>
       <HorizontalBarContent>
@@ -142,16 +130,21 @@ const HorizontalFilterBar: React.FC<HorizontalBarProps> = ({
           <Loading position="inline-centered" />
         ) : (
           <>
-            <FilterBarSettings />
-            {canEdit && isFeatureEnabled(FeatureFlag.DASHBOARD_NATIVE_FILTERS) && (
-              <FiltersLinkContainer hasFilters={hasFilters}>
-                <FilterConfigurationLink
-                  dashboardId={dashboardId}
-                  createNewOnOpen={filterValues.length === 0}
-                >
-                  <Icons.PlusSmall /> {t('Add/Edit Filters')}
-                </FilterConfigurationLink>
-              </FiltersLinkContainer>
+            {!isKatalonEmbeddedMode && (
+              <>
+                <FilterBarSettings />
+                {canEdit &&
+                  isFeatureEnabled(FeatureFlag.DASHBOARD_NATIVE_FILTERS) && (
+                    <FiltersLinkContainer hasFilters={hasFilters}>
+                      <FilterConfigurationLink
+                        dashboardId={dashboardId}
+                        createNewOnOpen={filterValues.length === 0}
+                      >
+                        <Icons.PlusSmall /> {t('Add/Edit Filters')}
+                      </FilterConfigurationLink>
+                    </FiltersLinkContainer>
+                  )}
+              </>
             )}
             {!hasFilters && (
               <FilterBarEmptyStateContainer data-test="horizontal-filterbar-empty">
